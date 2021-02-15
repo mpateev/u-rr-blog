a8a7887def1d20f459467ff5659864d40d3831b2
murad.pateev <mpateev@gmail.com>
2021-02-15 17:14:01 +0100

    post list displayed

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,8 +1,13 @@
+// posts reducer will receive current state as [] and action as { type, payload }
+// and return new state object as [] based on the current value and action type and payload
+//
 const postsReducer = (state = [], action) => {
     switch (action.type) {
         case 'FETCH_POSTS':
+            // got array of fetched posts as payload
             return action.payload;
         default:
+            // no type defined - just return the current state or [] as initial value
             return state;
     }
 }
