093fd9a4c9c2da95adf8b7a5630bd5958437951d
Murad Pateev <mpateev@gmail.com>
2021-02-17 11:58:07 +0100

    lesson completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,39 @@
-import jsonPlaceholder from '../apis/jsonPlaceholder'
+import _ from "lodash";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-// action creator for loading posts from api, using thunk middleware
-//
-export const fetchPosts = () => async dispatch => {
-    const response = await jsonPlaceholder.get('/posts');
+// * action creators for fetching posts and users lists
+// * fetchPostsAndUsers(), fetchPosts() and fetchUser()
+// Using Redux-Thunk as middleware for Redux store, we allowed to use  async requests to API using
+// fetch() or axios(). Since a redux store was created using a thunk middleware, the action creator can
+// return a function as well as action object.  The function will be automatically called with dispatch
+// and getState functions as first and second arguments - it's up to the function to dispatch appropriate
+// action to the redux store.
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  //*  "classic" version using lodash functions _.map() and _.uniq()
+  //   const userIds = _.uniq(_.map(getState().posts, "userId"));
+  //   userIds.forEach((id) => dispatch(fetchUser(id)));
+  //
+  //*  alternate version using _.chain() function from lodash
+  _.chain(getState().posts) // chain posts list
+    .map("userId") // as 1st argument to _.map() and 'userId' as 2nd
+    .uniq() // call _.uniq() for resulting array and then
+    .forEach((id) => dispatch(fetchUser(id))) // forEach() to resulting array
+    .value(); // finally execute the whole chain with _.value()
+};
 
-    dispatch({ type: 'FETCH_POSTS', payload: response.data });
-}
+// action creator for loading posts from api, with thunk middleware
+// will return a function to be dispatch to redux-thunk
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+// action creator for loading single user by id from api, with thunk middleware
+// will return a function to be dispatch to redux-thunk
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { connect } from 'react-redux'
-import { fetchPosts } from '../actions'
+import { connect } from "react-redux";
+import { fetchPostsAndUsers } from "../actions";
+import UserHeader from "./UserHeader";
 
 class PostList extends React.Component {
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
 
   renderList() {
@@ -17,26 +18,19 @@ class PostList extends React.Component {
               <h3>{post.title}</h3>
               <p>{post.body}</p>
             </div>
+            <UserHeader userId={post.userId} />
           </div>
         </div>
-      )
-    })
+      );
+    });
   }
   render() {
-    return (
-      <div className="ui relaxed divided list" >
-        { this.renderList()}
-
-
-      </div>)
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = (state) => {
   return { posts: state.posts };
-}
+};
 
-export default connect(
-  mapStateToProps,
-  { fetchPosts }
-)(PostList);
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList);
